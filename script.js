ebca17a362237e351106e6e4c704c5320f6b49ab
creeperwanhman <creeperwanhman@gmail.com>
2025-08-08 19:31:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Welcome to Bramuel Barasa's Personal Website!");
+  });
+  
+<script>
+        // Alert on form submission
+        document.querySelector("form").addEventListener("submit",function(e) {
+            e.preventDefault();
+            alert("Message sent!");
+        });
+    </script>
